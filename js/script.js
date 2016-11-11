567fe9d981e33f2cba78126805ee1acfee79bf6b
Kylie <kyliepace@gmail.com>
2016-11-11 09:58:46 -0800

    sends coordinates to usgs server to calculate water basin

--- js/script.js
+++ js/script.js
@@ -25,12 +25,39 @@ require([
           map.on("click", showCoordinates);
         });
 
+        var usgsBasinUrl;
         function showCoordinates(evt) {
           //the map is in web mercator but display coordinates in geographic (lat, long)
           var mp = webMercatorUtils.webMercatorToGeographic(evt.mapPoint);
           //display mouse coordinates
-          dom.byId("info").innerHTML = mp.x.toFixed(3) + ", " + mp.y.toFixed(3);
+          dom.byId("container1").innerHTML = "Calculate basin from "+ mp.x.toFixed(5) + ", " + mp.y.toFixed(5);
+          dom.byId("container1").style.display = "inline";
+          usgsBasinUrl = 'http://streamstatsags.cr.usgs.gov/streamstatsservices/watershed.json?rcode=CA&xlocation='+mp.x.toFixed(5)+'&ylocation='+mp.y.toFixed(5)+'&crs=3310&includeparameters=false&includeflowtypes=false&includefeatures=true&simplify=true'
+        
+          document.getElementById("container1").addEventListener("click", function(){
+          	request.open("Get", usgsBasinUrl);
+          	request.send(); //send request to usgs to return water basin characteristics
+          	//make page show that it's loading data 
+          	this.disabled = "disabled"; //disable button
+          });
+          //can I show the returned polygon on the map and change the "container1" button's text and function?
         };
     });
 });
 
+
+var request = new XMLHttpRequest();
+ 
+request.onreadystatechange = function() {
+  if(request.readyState === 4) { //if response is ready
+ 	
+    if(request.status === 200) { 
+      //what to do with successful response
+      var response = JSON.parse(request.responseText);
+      console.log(response);
+      return response;
+    } else {
+      document.getElementById('info').innerHTML = 'An error occurred during your request: ' +  request.status + ' ' + request.statusText;
+    } 
+  }
+};
