8e2dbe26ba8ef7209312d5b5f0292516a8853f96
Kylie <kyliepace@gmail.com>
2016-11-16 11:31:44 -0800

    refactored code with local data

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ require([
     "esri/map",
     "esri/layers/ArcGISTiledMapServiceLayer",
    	"esri/geometry/webMercatorUtils", 
+   	"esri/SpatialReference",
    	"esri/geometry/Polygon",
    	"esri/symbols/SimpleFillSymbol",
    	"esri/symbols/SimpleLineSymbol", "esri/Color",
@@ -17,6 +18,7 @@ require([
     Map,
     ArcGISTiledMapServiceLayer,
    	webMercatorUtils, 
+   	SpatialReference,
    	Polygon,
    	SimpleFillSymbol,
    	SimpleLineSymbol, Color,
@@ -30,10 +32,21 @@ require([
 	        basemap: "topo",
 	        center: [-119.4179,36.7783],
 	        zoom: 7,
-	        smartNavigation: false
+	        smartNavigation: false,
+	        spatialReference: 3310
 	    });
 	    console.log('created map');
-
+	    polygon = new Polygon();
+	    fillSymbol = new SimpleFillSymbol(
+      		SimpleFillSymbol.STYLE_SOLID,
+			new SimpleLineSymbol(
+					SimpleLineSymbol.STYLE_SOLID,
+					new Color([255,0,0,1]), 
+					200
+			),
+			new Color([255,255,0,1]) 
+		);
+		
 	    map.on("load", function() {
 	    	map.hidePanArrows();
 	    	map.showZoomSlider();
@@ -45,9 +58,15 @@ require([
 	    	showCoordinates(evt);
 	    });
 
+	    //when container1 is clicked, send coordinates to usgs to return watershed info
 	    document.getElementById("container1").addEventListener("click", function(){
 	    	this.disabled = "disabled";
-	   //  	request.open("Get", usgsBasinUrl);
+	    	usgsRequest(); 
+	    });
+    };
+
+    var usgsRequest = function(){
+		//request.open("Get", usgsBasinUrl);
 	   //  	request.send();
 	   //  	request.onreadystatechange = function(){
 	   //  		var update = document.getElementById("container2");
@@ -71,11 +90,9 @@ require([
 			 //  		update.innerHTML = "Retrieving watershed data from USGS";
 			 //  		document.body.style.cursor = "wait";
 			 //  	} 
-			 // };  
-	    });
+			 // }; 
     };
-
-    function showCoordinates(evt, map) {
+    function showCoordinates(evt) {
     	//the map is in web mercator but display coordinates in geographic (lat, long)
         var mp = webMercatorUtils.webMercatorToGeographic(evt.mapPoint);
         //display mouse coordinates
@@ -85,30 +102,13 @@ require([
         usgsBasinUrl = 'http://streamstatsags.cr.usgs.gov/streamstatsservices/watershed.json?rcode=CA&xlocation='+mp.x.toFixed(5)+'&ylocation='+mp.y.toFixed(5)+'&crs=3310&includeparameters=false&includeflowtypes=false&includefeatures=true&simplify=true';
     };
 
-    function drawWatershed(){
-    	var rings = waterbasin.featurecollection[1].feature.features[0].geometry.rings[0];
-    	console.log(map);
-  		/********************************************
-        * Create a polygon graphic of the watershed *
-        *********************************************/
-   		//var polygonJson  = {"rings":rings,"spatialReference":{"wkid":3310 }};
-			var polygon = new Polygon();
-			polygon.addRing(rings);
-			polygon.spatialReference = map.spatialReference;
+    var drawWatershed = function(){
+    	var rings = waterbasin.featurecollection[1].feature.features[0].geometry.rings[0]; 
+		polygon.addRing(rings);
+		polygon.spatialReference = map.spatialReference;
       	// Create a symbol for rendering the graphic
-      	var fillSymbol = new SimpleFillSymbol(
-      		SimpleFillSymbol.STYLE_SOLID,
-			new SimpleLineSymbol(
-					SimpleLineSymbol.STYLE_DASHDOT,
-					new Color([255,0,0]), 
-					200
-			),
-			new Color([255,255,0,0.7])
-		);
- 
-        var watershedGraphic = new Graphic(polygon, fillSymbol, {keeper: true}) ;
-
-        var watershedLayer = new GraphicsLayer({id: "basin"});  
+        watershedGraphic = new Graphic(polygon, fillSymbol, {keeper: true}) ;
+        watershedLayer = new GraphicsLayer({id: "basin"});  
         watershedLayer.setScaleRange(0,0); //make sure it's visible at all scales
         watershedLayer.setVisibility(true);
         map.addLayer(watershedLayer);
@@ -116,15 +116,18 @@ require([
         console.log('added layer to map');
         watershedLayer.add(watershedGraphic);
         console.log('added graphic to layer');
-        console.log(watershedLayer);
         watershedLayer.redraw();
     };
 
-    ready(initialize);
+    ready(initialize); //run function
 });
 
 var map = null;
 var usgsBasinUrl;
 var watershed;
+var polygon;
+var fillSymbol;
 var watershedID;
+var watershedGraphic;
+var watershedLayer;
 var request = new XMLHttpRequest();
