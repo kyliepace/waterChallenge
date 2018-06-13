6e04565c57af639842dffe7bee716be07a67a5c1
Kylie <kyliepace@gmail.com>
2018-06-13 14:22:06 -0700

    single webpack to rule them all

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,10 @@
 'use strict';
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  //router.get('/', server.loopback.status());
   server.use(router);
 };
