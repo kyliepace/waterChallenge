2337fbf87ea9f5bba1d0fdf0cf413e9c9f5c58b3
Kylie <kyliepace@gmail.com>
2018-07-01 11:46:55 -0700

    goodbye loopback, just using express

--- server.js
+++ server.js
@@ -1,31 +1,22 @@
 'use strict';
+const express = require("express");
+const dotenv = require('dotenv');
+const bodyParser = require("body-parser");
+//const routes = require('./server/routes');
+const expressStaticGzip = require("express-static-gzip");
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
-var dotenv = require('dotenv');
-dotenv.config()
+let server = express();
+dotenv.config();
+server.use(bodyParser.json());
+server.use("/", expressStaticGzip("client/dist"));
+server.use(express.static("client/dist"));
 
-var app = module.exports = loopback();
-
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
-
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
-
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+let port = process.env.PORT || '3000';
+server.listen(port, () => {
+  console.log(
+    "Starting server in " +
+      process.env.NODE_ENV +
+      " mode: Listening on " +
+      port
+  );
 });
