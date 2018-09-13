4db937e10ea40f8022ed80120c428d0fccc4d459
Kylie Pace <kyliepace@gmail.com>
2018-09-13 15:27:37 -0700

    not sure how to make final query most efficient so just force it for now

--- server/sum-face-values.js
+++ server/sum-face-values.js
@@ -5,36 +5,50 @@ const db = require('./db.js');
 module.exports = (basin, points) => {
   db.connect()
   .then(client => {
-    let geomText = `POLYGON((${basin}))`;
-    // do I need to use ST_GeomFromText?
-    return client.query(`
-      SELECT pod, acf
-      FROM ewrims
-      WHERE ST_Within(ewrims.pod, ${geomText})
-    `)
-    .then((rights) => {
-      Promise.all(points.map(point => {
-        // find all results within the point.basin
-        let basinText = `POLYGON((${point.basin}))`;
 
-        // will need to flatten rights.rows into an array of the pod values
-        let rightsText = `MULTIPOINT(${rights.rows.pod})`;
+    Promise.all(points.map(point => {
 
-        return client.query(`
-          SELECT rights
-          FROM (
-            SELECT ST_GeomFromText(${basinText}) AS basin,
-            SELECT ST_GeomFromText(${rightsText}) AS rights
-          )
-          WHERE ST_Within(rights, basin);
-        `)
-      }))
-      .then(res => {
-        return res.rows;
-      })
-      .catch(err => {
-        console.log(err);
-      });
+      // do I need to use ST_GeomFromText?
+      let geomText = `POLYGON((${point.basin}))`;
+
+      // find points within each basin
+      // there will be some redundancy here...possible refactor
+      return client.query(`
+        SELECT pod, acf
+        FROM ewrims
+        WHERE ST_Within(ewrims.pod, ${geomText})
+      `)
+    }))
+    .then(res => {
+      client.release();
+      return res.rows;
+    })
+    // .then((rights) => {
+    //   Promise.all(points.map(point => {
+    //     // find all results within the point.basin
+    //     let basinText = `POLYGON((${point.basin}))`;
+
+    //     // will need to flatten rights.rows into an array of the pod values
+    //     let rightsText = `MULTIPOINT(${rights.rows.pod})`;
+
+    //     return client.query(`
+    //       SELECT rights
+    //       FROM (
+    //         SELECT ST_GeomFromText(${basinText}) AS basin,
+    //         SELECT ST_GeomFromText(${rightsText}) AS rights
+    //       )
+    //       WHERE ST_Within(
+    //         basin,
+    //         SELECT ST_GeomFromText(${basinText})
+    //       );
+    //     `)
+    //   }))
+      // .then(res => {
+      //   return res.rows;
+      // })
+      // .catch(err => {
+      //   console.log(err);
+      // });
     })
     .catch(err => {
       client.release();
